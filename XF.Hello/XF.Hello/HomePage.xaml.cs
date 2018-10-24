9930d3a5c6c04b86eeff6a1704fbd7be51099a79
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-23 21:23:51 -0300

    Add project files.

---
+++ XF.Hello/XF.Hello/HomePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Hello
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+		}
+
+        void OnAlterarIntensidadeSlider(Object sender, EventArgs e)
+        {
+            var vermelho = sliderVermelho.Value;
+            var verde = sliderVerde.Value;
+            var azul = sliderAzul.Value;
+
+            boxviewCor.Color = Color.FromRgb(vermelho, verde, azul);
+        }
+    }
+}
